855094809f8dc8e2b9f3e89457f092a0e65e76d4
Nicholas Weyenberg <80421977+weyenic12@users.noreply.github.com>
2021-05-05 19:57:18 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,11 +35,11 @@ e.birth_date,
 de.from_date,
 de.to_date,
 t.title
-INTO membership_eligibility
+INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp as de ON (e.emp_no=de.emp_no)
 INNER JOIN titles as t ON (e.emp_no=t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
-SELECT * FROM membership_eligibility
+SELECT * FROM mentorship_eligibility
 
